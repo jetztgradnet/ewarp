62374a4b955d18cade88a3d6cb764149271d787c
Wolfgang Schell <github@jetztgrad.net>
2011-07-13 02:39:50 +0200

    only destroy runtime if it was created by this servlet

--- src/main/java/erjangx/ewarp/web/ErjangServlet.java
+++ src/main/java/erjangx/ewarp/web/ErjangServlet.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServlet;
  */
 public class ErjangServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	public final static String ATTRIBUTE_INITIALZED_BY_SERVLET = "erjangx.ewarp.web.INITIALZED_BY_SERVLET";
 
 	/* (non-Javadoc)
 	 * @see javax.servlet.GenericServlet#init(javax.servlet.ServletConfig)
@@ -29,6 +31,7 @@ public class ErjangServlet extends HttpServlet {
 		ErjangWebRuntime runtime = ErjangWebRuntime.getErjangRuntime(context);
 		if (runtime == null) {
 			runtime = new ErjangWebRuntime(config, context);
+			context.setAttribute(ATTRIBUTE_INITIALZED_BY_SERVLET, getServletName());
 		}
 		
 		runtime.start();
@@ -40,9 +43,17 @@ public class ErjangServlet extends HttpServlet {
 	@Override
 	public void destroy() {
 		ServletContext context = getServletContext();
-		ErjangWebRuntime runtime = ErjangWebRuntime.getErjangRuntime(context);
-		if (runtime != null) {
-			runtime.shutdown();
+		
+		// determine whether the Erjang runtime was initialized by this servlet
+		Object name = context.getAttribute(ATTRIBUTE_INITIALZED_BY_SERVLET);
+		if ((name != null)
+			&& getServletName().equals(name)) {
+			// Erjang runtime was initialized by this servlet, so we shut it down
+			ErjangWebRuntime runtime = ErjangWebRuntime.getErjangRuntime(context);
+			if (runtime != null) {
+				runtime.shutdown();
+				context.removeAttribute(ATTRIBUTE_INITIALZED_BY_SERVLET);
+			}
 		}
 		
 		super.destroy();
