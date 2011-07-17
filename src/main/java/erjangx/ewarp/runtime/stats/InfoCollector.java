05a93999ba7dd742206135ccb9e5d0f0a6c2ecdd
Wolfgang Schell <github@jetztgrad.net>
2011-07-17 18:12:06 +0200

    added local node name

--- src/main/java/erjangx/ewarp/runtime/stats/InfoCollector.java
+++ src/main/java/erjangx/ewarp/runtime/stats/InfoCollector.java
@@ -4,9 +4,11 @@ import java.util.Map;
 
 import erjang.EModuleManager;
 import erjang.EProc;
+import erjang.m.erlang.ErlDist;
 import erjangx.ewarp.runtime.ErjangRuntime;
 
 public class InfoCollector extends AbstractStatusCollector {
+	public final StatusName NODE_NAME = new StatusName("node_name", "Node name");
 	public final StatusName PROCESS_COUNT = new StatusName("process_count", "Process count");
 	public final StatusName MODULE_COUNT = new StatusName("module_count", "Module count");
 
@@ -19,6 +21,7 @@ public class InfoCollector extends AbstractStatusCollector {
 	 */
 	public boolean collectStatus(ErjangRuntime runtime,
 			Map<StatusName, Object> status) {
+		collect(status, NODE_NAME, ErlDist.node().getName());
 		collect(status, PROCESS_COUNT, EProc.process_count());
 		collect(status, MODULE_COUNT, EModuleManager.loaded_modules().length());
 		return true;
